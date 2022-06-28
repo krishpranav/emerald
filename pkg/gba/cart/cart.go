8d03a9ca343648509dc4aeb69289686c249e2402
krishpranav <krisna.pranav@gmail.com>
2022-06-28 16:51:56 +0530

    pkg: gba cart

--- pkg/gba/cart/cart.go
+++ pkg/gba/cart/cart.go
@@ -3,22 +3,24 @@ package cart
 import "fmt"
 
 type Header struct {
-	Entry      [4]byte
-	Title      string
-	GameCode   string
-	MarkerCode string
+	Entry     [4]byte
+	Title     string
+	GameCode  string
+	MakerCode string
 }
 
 func New(src []byte) *Header {
 	return &Header{
-		Entry:      [4]byte{src[0], src[1], src[2], src[3]},
-		Title:      string(src[0xa0 : 0xa0+12]),
-		GameCode:   string(src[0xac : 0xac+4]),
-		MarkerCode: string(src[0xac : 0xac+2]),
+		Entry:     [4]byte{src[0], src[1], src[2], src[3]},
+		Title:     string(src[0xa0 : 0xa0+12]),
+		GameCode:  string(src[0xac : 0xac+4]),
+		MakerCode: string(src[0xb0 : 0xb0+2]),
 	}
 }
 
 func (h *Header) String() string {
-	str := `Title: \nGameCode: %s`
-	return fmt.Sprintf(str, h.Title, h.GameCode, h.MarkerCode)
+	str := `Title: %s
+GameCode: %s
+MakerCode: %s`
+	return fmt.Sprintf(str, h.Title, h.GameCode, h.MakerCode)
 }
